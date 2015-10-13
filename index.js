490035ae5f984eb81adc9a358d933c94dba0ebf0
Vitaliy Stepanyuk <vstepanyuk@me.com>
2015-10-14 00:39:56 +0200

    Added title option

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 const Ouch = require('ouch')
 
 module.exports = function (app, options) {
-  const opts = Object.assign({theme: 'orange'}, options || {})
+  const opts = Object.assign({
+    theme: 'orange',
+    title: null
+  }, options || {})
 
   app.context.onerror = function (err) {
     // If there is no error then skip
     if (!err) return;
 
     const ouch = new Ouch()
-    ouch.pushHandler(new Ouch.handlers.PrettyPageHandler(opts.theme))
+    ouch.pushHandler(new Ouch.handlers.PrettyPageHandler(opts.theme, opts.title))
     ouch.handleException(err, this.req, this.res)
   }
 }
